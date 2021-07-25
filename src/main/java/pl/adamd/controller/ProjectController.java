f276d6f3e676c5bfe199fbd39728934af6ffba19
Adam <adam.drozdz1987@gmail.com>
2021-07-25 23:04:29 +0200

    Dependencies has been added to connect with keycloak, create package security with SecurityConfiguration.java with annotation @KeycloakConfiguration and authentication strategy,  and SsoController.java. In application-local.yml configured connection with keycloak not commit to github - local environment.

--- src/main/java/pl/adamd/controller/ProjectController.java
+++ src/main/java/pl/adamd/controller/ProjectController.java
@@ -2,6 +2,8 @@ package pl.adamd.controller;
 
 import io.micrometer.core.annotation.Timed;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,10 +14,12 @@ import pl.adamd.model.ProjectStep;
 import pl.adamd.model.projection.ProjectWriteModel;
 
 import javax.validation.Valid;
+import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 @RequestMapping("/projects")
 class ProjectController {
     private final ProjectService service;
@@ -25,9 +29,12 @@ class ProjectController {
     }
 
     @GetMapping
-    String showProjects(Model model) {
-        model.addAttribute("project", new ProjectWriteModel());
-        return "projects";
+    String showProjects(Model model, Authentication auth) {
+//        if (auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))) {
+            model.addAttribute("project", new ProjectWriteModel());
+            return "projects";
+//        }
+//        return "index";
     }
 
     @PostMapping
